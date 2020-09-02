42ebf9ee3db5584dc55acd4b9915f2a53f12ff04
Simon Li <zylix666@gmail.com>
2020-09-02 23:42:30 +0800

    Using logging module
    No more print....At least in entry file.

--- run.py
+++ run.py
@@ -8,30 +8,43 @@ Created on Wed Jul  1 18:16:13 2020
 """
 
 import sys
-import logging
- 
+
 from stock_history_retriever import Stock_history_retriever
 from Messenger_Line import Messenger_Line
 from utils import query_stock_company_names
 
 sys.path.append(".")
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+    filename="TW_stock_log.txt", \
+    format='[%(asctime)s %(levelname)-8s] %(message)s', \
+	datefmt='%Y%m%d %H:%M:%S')
+
+
+
 if __name__ == "__main__":
-    logger = logging.getLogger('logging_main.logging_core')
+    logger = logging.getLogger(__name__)
     stock_number_list = ["0050","2002", "2330"]
     name_code = query_stock_company_names(stock_number_list)
+    logger.debug("Stock number and company name mapping loaded!")
 
     show_fields = ["close", "change"]
-    stock_to_show_dict={}
+    stock_to_show_dict = {}
+
     for stock_no in stock_number_list:
+        logger.debug("Retrieve the stock - " + stock_no + " history.")
         shr_obj = Stock_history_retriever(stock_no, load_hist=True)
-        #shr_obj.grab_two_years()
+        # shr_obj.grab_two_years()
+        logger.debug("Retrieve the stock - " + stock_no + " today's record.")
         show_list = shr_obj.grab_to_show(show_fields)
-        stock_to_show_dict.update({stock_no:show_list})
- 
+        logger.info("Grabbed today's stock trade record. Update showing data")
+        stock_to_show_dict.update({stock_no: show_list})
+
+    logger.info("Initial messenger.")
     messenger_obj = Messenger_Line(name_code, stock_to_show_dict)
     message = messenger_obj.compose_messge()
     token = "YOUR LINE NOTIFY TOKEN"
-    print(message)
+    logger.debug(message)
     messenger_obj.lineNotifyMessage(token, message)
     
